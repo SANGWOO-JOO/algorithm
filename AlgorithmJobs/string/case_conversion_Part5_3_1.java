992ea553df33b8358acdaf565de4000d1070e69a
SANGWOO-JOO <turncoat19@gmail.com>
2022-09-19 23:44:09 +0900

    algorithm

---
+++ AlgorithmJobs/string/case_conversion_Part5_3_1.java
@@ -0,0 +1,20 @@
+package string;
+
+import java.util.Scanner;
+
+public class case_conversion_Part5_3_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char arr[] =new char [1000];
+		String word ;
+		Scanner sc =new Scanner(System.in);
+		
+		word = sc.nextLine();
+		
+		word = word.toLowerCase();
+		
+		System.out.print(word);
+	}
+
+}
