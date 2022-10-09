ffe497cfdce9b5694fb8c7634c8e3d06bdb520d7
SANGWOO-JOO <turncoat19@gmail.com>
2022-10-09 23:35:07 +0900

    algorithm

--- AlgorithmJobs/recursive_function/Study.java
+++ AlgorithmJobs/recursive_function/Study.java
@@ -2,9 +2,17 @@ package recursive_function;
 
 public class Study {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static void main(String [] args){
+        int result = tailRecursive(5, 0);
+ 		System.out.println(result);
+    }
+    
+	public static int tailRecursive(int n, int acc)
+	{
+ 		if(n==1) {
+        	return acc;
+    	} 
+ 		return tailRecursive(n-1, n + acc );        
+    }
 
 }
