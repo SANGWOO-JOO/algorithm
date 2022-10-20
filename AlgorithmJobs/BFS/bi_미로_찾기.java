3ceec25790e861c86f26625eb6bf5636948e28c9
SANGWOO-JOO <turncoat19@gmail.com>
2022-10-20 23:23:38 +0900

    BFS, 다익스트라

--- AlgorithmJobs/BFS/bi_미로_찾기.java
+++ AlgorithmJobs/BFS/bi_미로_찾기.java
@@ -10,9 +10,7 @@ public class bi_미로_찾기 {
      private static int m;
 
      private static int[][] arr;
-     private static int[][] dist;
-     private static boolean[][] isNotBroken;
-     private static boolean[][] isBroken;
+     private static boolean[][] isvisited;
  
      private static int[] dy = {-1, 1, 0, 0};
      private static int[] dx = {0, 0, -1, 1};
@@ -22,55 +20,45 @@ public class bi_미로_찾기 {
 
 		Scanner sc = new Scanner(System.in);
 		
-		n = sc.nextInt();
-		m =sc.nextInt();
+		n = sc.nextInt(); //세로
+		m =sc.nextInt(); //가로
 		
 		arr = new int[n][m];
-        dist = new int[n][m];
-        isNotBroken = new boolean[n][m];
-        isBroken = new boolean[n][m];
+		isvisited = new boolean[n][m];
         
-        for(int i = 0; i < n; i++) {
+            for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
         
         bfs(n-1, 0);
+        System.out.println(arr[0][m-1]);
 	}
 
 	private static void bfs(int i, int j) {
 		// TODO Auto-generated method stub
 		 Queue<int[]> queue = new LinkedList<>();
-         queue.add(new int[] {i, j, 0}); // 0은 한번도 부수지 않은 상태, 1은 한번 부순 상태
-         isNotBroken[i][j] = true;
+         queue.add(new int[] {i,j});
+         isvisited[i][j]=true;
          
          while(!queue.isEmpty()) {
-             if(dist[0][m-1] != 0 ) break;
-             int[] w = queue.poll();
-             for(int k = 0; k < 4; k++) {
-                 int y = w[0] + dy[k];
-                 int x = w[1] + dx[k];
-                 int b = w[2];
-
-                 if(y >= 0 && y < n && x >=0 && x < m) {
-                     if(b == 1) { // 벽을 이미 한번 부순 상태
-                         if(!isNotBroken[y][x] && !isBroken[y][x] && arr[y][x] == 0) {
-                             queue.add(new int[]{y, x, 1});
-                             dist[y][x] = dist[w[0]][w[1]] + 1;
-                             isBroken[y][x] = true;
-                         }
-                     } else { // 한번도 벽을 부수지 않은 상태
-                         if(!isNotBroken[y][x]) {
-                             if(arr[y][x] == 0) queue.add(new int[] {y, x, 0});
-                             else if(arr[y][x] == 1) queue.add(new int[] {y, x, 1});
-                             dist[y][x] = dist[w[0]][w[1]] + 1;
-                             isNotBroken[y][x] = true;
-                         }
-                     }
-                 }
-             }
+        	 int[]w =queue.poll();
+        	 
+        	 for(int k=0;k<4;k++) {
+        		 int y=w[0]+dy[k];
+        		 int x=w[1]+dx[k];
+        		 
+        		 if(y>=0&&y<n&&x>=0&&x<m) {
+        			 if(arr[y][x]==0&&!isvisited[y][x]) {
+        				 queue.add(new int [] {y,x});
+        				 isvisited[y][x]=true;
+        				 arr[y][x] = arr[w[0]][w[1]]+1;
+        			 }
+        		 }
+        		 
+        	 }
          }
 	}
-
 }
+
--- AlgorithmJobs/BFS/bi_이상한_계산기.java
+++ AlgorithmJobs/BFS/bi_이상한_계산기.java
@@ -4,40 +4,48 @@ import java.io.*;
 import java.util.*;
 
 public class bi_이상한_계산기 {
-	
+	private static int n;
+    private static boolean[] isvisited = new boolean[1000000];
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	
-		 Scanner sc = new Scanner(System.in); 
-         int target = sc.nextInt(); //10
 
-         if(target==1) System.out.println("0");
-         else if(target==0) System.out.println("1");
-         else System.out.println( bps(target) );
+	 Scanner sc = new Scanner(System.in); 
+	 n =sc.nextInt();
+	
+	 if(n==1) System.out.println("0");
+     else if(n==0) System.out.println("1");
+     else System.out.println( bfs(n) );
 	}
 
-	private static int bps(int target) {
+	private static int bfs(int v) {
 		// TODO Auto-generated method stub
-		
 		Queue<Integer> queue = new LinkedList<>();
-        queue.offer(1);
-        
-        int level = 0;
-        
-        while (!queue.isEmpty()){
-        	 int qSize = queue.size();
-             for(int i=0; i<qSize; i++){
-            	 
-        	 int currentNum = queue.poll();
-        	 
-        	 if(currentNum==target) return level;
-             if(currentNum * 2 < 100000) queue.offer(currentNum*2 );
-             if(currentNum/3 != 0) queue.offer( currentNum/3 );
-                         	 
-             }
-             level++;
-        }
-        return -1;
+        queue.add(1);
+        int level=0;
+
+        while(!queue.isEmpty()) {
+            
+        	int qSize = queue.size();
+            for(int i=0; i<qSize; i++){
+
+                int currentNum = queue.poll();
+                if(currentNum==n) return level;
+                
+                if( currentNum*2 < 100000 && !isvisited[currentNum*2] ){
+                	isvisited[currentNum*2]=true;
+                    queue.offer(currentNum*2 );
+                }
+
+                if( currentNum/3 != 0 && !isvisited[currentNum/3] ){
+                	isvisited[currentNum/3]=true;
+                    queue.offer( currentNum/3 );
+                }
+            }
+            level++;
+            }
+		return -1;
+            
+            
+            
 	}
 }
