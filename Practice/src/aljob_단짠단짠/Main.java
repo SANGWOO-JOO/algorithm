c5e94b7d831cfa9f1054cdf179fa0742d0abcb99
SANGWOO-JOO <turncoat19@gmail.com>
2022-11-28 21:27:19 +0900

    algorithm

---
+++ Practice/src/aljob_단짠단짠/Main.java
@@ -0,0 +1,36 @@
+package aljob_단짠단짠;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //입력
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int danjan[] = new int[n]; // 단짠 배열
+
+        for (int i = 0; i < n; i++) {
+
+            danjan[i] = scan.nextInt();
+        }
+
+        Arrays.sort(danjan); // 정렬
+
+        int cnt = 0;  // 카운트
+
+        int e = n - 1;
+        int zero=0;
+        int sum=0;
+
+    
+
+        }
+
+
+
+
+
+    }
+}
