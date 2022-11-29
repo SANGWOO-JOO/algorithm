4b3da89977d4dbaded0ba904d097bf8d4c19d9d6
SANGWOO-JOO <turncoat19@gmail.com>
2022-11-29 21:06:12 +0900

    algorithm

--- Practice/src/aljob_단짠단짠/Main.java
+++ Practice/src/aljob_단짠단짠/Main.java
@@ -1,35 +1,43 @@
 package aljob_단짠단짠;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
+
+
         //입력
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+        int n = scan.nextInt();  //수열의 길이
 
-        int danjan[] = new int[n]; // 단짠 배열
+        long cnt = 0;            //수열의 합이 k가 되는 경우의 수 카운트
+        int dp[] = new int[n + 1];  //1번 인덱스부터 i번 인덱스까지의 수열의 합
+        Map<Integer, Long> map = new HashMap<>();   //1번부터 i번까지의 구간 합의 개수
 
-        for (int i = 0; i < n; i++) {
 
-            danjan[i] = scan.nextInt();
+        //부분 수열의 합이 0가 되는 경우의 수 카운트
+        for (int i = 1; i <= n; i++) {
+            dp[i] =  dp[i - 1] + scan.nextInt();   //dp[i] = dp[i-1] + arr[i]
+
+            if(dp[i] == 0){   //arr[1]~arr[i]까지의 합이 0와 같으면
+                cnt++;
+            }
+            if(map.containsKey(dp[i] - 0)==true){
+                cnt += map.get(dp[i] - 0);
+            }
+
+            if(map.containsKey(dp[i])==false) {
+                map.put(dp[i], 1L);
+            }
+            else {
+                map.put(dp[i], map.get(dp[i]) + 1);
+            }
         }
 
-        Arrays.sort(danjan); // 정렬
-
-        int cnt = 0;  // 카운트
-
-        int e = n - 1;
-        int zero=0;
-        int sum=0;
-
-    
-
-        }
-
-
-
+        System.out.println(cnt);  //수열의 합이 0이 되는 경우의 수 출력
 
 
     }
---
+++ Practice/src/aljob_단짠단짠/search.java
@@ -0,0 +1,38 @@
+package aljob_단짠단짠;
+
+import java.util.Scanner;
+
+public class search {
+
+    public static void main(String[] args) {
+
+        Scanner scan =new Scanner(System.in);
+        int a ;
+
+        a = scan.nextInt();
+
+        int b[] =new int[a];
+
+        for(int i=0;i<a;i++){
+            b[i]=scan.nextInt();
+//            System.out.print(b[i]);
+        }
+
+        int sum;
+        int cnt=0;
+        for(int i=0;i<a;i++){
+
+            sum =0;
+
+            for(int j=i;j<a;j++){
+
+                sum+=b[j];
+                if(sum==0)cnt++;
+               System.out.println(cnt);
+            }
+            System.out.println("-----");
+        }
+
+
+    }
+}
