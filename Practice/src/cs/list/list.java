46650f8dfef8840801f98bc0109a3f168de1aab0
SANGWOO-JOO <turncoat19@gmail.com>
2022-12-10 23:37:25 +0900

    algorithm

---
+++ Practice/src/cs/list/list.java
@@ -0,0 +1,17 @@
+package cs.list;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class list {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>(Arrays.asList("Black", "White", "Green", "Red"));
+        boolean contains = colors.contains("Black");
+        System.out.println(contains);
+
+        int index = colors.indexOf("Blue");
+        System.out.println(index);
+
+        index = colors.indexOf("Red");
+        System.out.println(index);
+    }
+}
