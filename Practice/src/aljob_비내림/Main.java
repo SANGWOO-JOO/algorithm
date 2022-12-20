4b767b589cfc4d378e6701d2cace95f8905236bd
SANGWOO-JOO <turncoat19@gmail.com>
2022-12-20 23:27:23 +0900

    algorithm

--- Practice/src/aljob_비내림/Main.java
+++ Practice/src/aljob_비내림/Main.java
@@ -1,24 +1,70 @@
 package aljob_비내림;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {42, 456, 7898, 156, 123, 77, 9, 498, 2142, 2, 3, 241, 65756, 553, 767};
 
-        for(int i=0; i<arr.length; i++) {
-            for(int j=i+1; j<arr.length; j++) {
-                if(arr[i] > arr[j]) { //오름차순
-                    int tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        int[] arr = new int[n]; // 배열 길이 정하기
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt(); // 한줄식 넣기
+        }
+
+//        System.out.println("입력 값= " + arr[0] );
+//        System.out.println("입력 값= " + arr[1] );
+//        System.out.println("입력 값= " + arr[2] );
+//        System.out.println("입력 값= " + arr[3] );
+
+
+
+
+          /*
+           1.  arr[0]과 arr[1]을 비교 arr[0] > arr[1] 이면 arr[0] -1 을 한다.
+           2.  arr[0] == arr[5]이 되면 나온다.
+           3.  그리고 cnt 카운트
+             */
+
+
+//        for(int i=0; i<n-1; i++){
+//            for(int j=1; i<n;j++){
+//
+//            }
+//        }
+
+//        if(arr[0]>arr[1]) arr[0]--;
+
+//        System.out.println("입력 값= " + arr[0]);
+//        System.out.println("입력 값= " + arr[1]);
+//        System.out.println("입력 값= " + arr[2]);
+//        System.out.println("입력 값= " + arr[3]);
+
+//        System.out.println(cnt);
+
+        int cnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] >= arr[j]) { //오름차순
+                    arr[i]--;
+                    cnt++;
+                    j --;
+//                    System.out.println("j의 인덱스" +j);
+                } else {
+                    continue;
                 }
+//                System.out.println("-----------");
             }
         }
 
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i]);
-            if(i+1 != arr.length) {
-                System.out.print(", ");
-            }
-        }
+//        for(int i=0;i<4;i++){
+//            System.out.println(arr[i]);
+//        }
+//        System.out.println("----");
+        System.out.println(cnt);
     }
 }
