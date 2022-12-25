6bb61b68ec6f544491c37bae23c1a037e14aed2b
SANGWOO-JOO <turncoat19@gmail.com>
2022-12-25 23:32:15 +0900

    algorithm

---
+++ Practice/src/aljob_팰린드롬/One.java
@@ -0,0 +1,23 @@
+package aljob_팰린드롬;
+
+import java.util.Scanner;
+
+public class One {
+
+    Scanner scan = new Scanner(System.in);
+
+    /*
+        abcefefabc  // 예제
+        1. 처음 입력하는 값을 넣어준다
+        2 abc a(처음) and c(끝) and e(처음) f(끝) 이렇게 처음 끝으로 되는 경우의 수를 구한다.
+        3 두에서
+
+     */
+
+    String str =scan.nextLine();// string 문자열 입력
+
+
+
+
+
+}
